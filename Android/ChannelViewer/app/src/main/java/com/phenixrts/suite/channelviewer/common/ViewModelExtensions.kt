8bb8ab735d5fb98fee1b8d643786b6b45857f6d7
Eddy Gorbunov <edijs.gorbunovs@phenixrts.com>
2022-02-04 14:24:39 +0200

    [Android] Fix ViewModelExtensions in ChannelViewer (#APP-933) Fixed
    * Fixed a bug in ViewModelExtensions.kt
    * Updated core libs;
    * Updated core lib - again;
    * Updated core lib - once again;
    
    Change-Id: Ifea2c3169180c01a552007fe2c9bd00a9d04820b
    Reviewed-on: https://gerrit.phenixrts.com/c/MobileChannelViewer/+/13358
    Reviewed-by: Edijs Gorbunovs <edijs.gorbunovs@phenixrts.com>
    Reviewed-by: Andreas Schuler <andreas.schuler@phenixrts.com>
    Tested-by: Edijs Gorbunovs <edijs.gorbunovs@phenixrts.com>
    Tested-by: Andreas Schuler <andreas.schuler@phenixrts.com>

--- Android/ChannelViewer/app/src/main/java/com/phenixrts/suite/channelviewer/common/ViewModelExtensions.kt
+++ Android/ChannelViewer/app/src/main/java/com/phenixrts/suite/channelviewer/common/ViewModelExtensions.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2021 Phenix Real Time Solutions, Inc. Confidential and Proprietary. All rights reserved.
+ * Copyright 2022 Phenix Real Time Solutions, Inc. Confidential and Proprietary. All rights reserved.
  */
 
 package com.phenixrts.suite.channelviewer.common
@@ -13,13 +13,13 @@ inline fun <reified T : ViewModel> lazyViewModel(
     noinline creator: (() -> T)? = null
 ) = lazy {
     if (creator == null)
-        ViewModelProvider(owner()).get(T::class.java)
+        ViewModelProvider(owner())[T::class.java]
     else
-        ViewModelProvider(owner(), BaseViewModelFactory(creator)).get(T::class.java)
+        ViewModelProvider(owner(), BaseViewModelFactory(creator))[T::class.java]
 }
 
 class BaseViewModelFactory<T>(val creator: () -> T) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
         @Suppress("UNCHECKED_CAST")
         return creator() as T
     }
